c79bdfac4a7736277abbc13c7b54d3214e7e9ad1
TechStudent11 <razaqhammed36@gmail.com>
2021-11-27 21:49:12 -0500

    add del command

---
+++ commands/del/__init__.py
@@ -0,0 +1,11 @@
+def command(os, config, args, kwargs):
+    if len(args) <= 0:
+        print("del: deletes stuff i guess")
+    
+    path = args[0]
+    if path in os.listdir():
+        if input(f'Are you sure you want to delete {path} (y/n)').lower() == 'y':
+            if os.path.isdir(path):
+                os.delete(path)
+            else:
+                os.rmdir(path)
--- commands/mkdir/__init__.py
+++ commands/mkdir/__init__.py
@@ -1,2 +1,6 @@
-def command():
-    print("mkdir() called")
+def command(os, config, args, kwargs):
+    if len(args) <= 0:
+        print("mkdir: makes a directory")
+
+    dir_name = args[0]
+    os.mkdir(dir_name)
